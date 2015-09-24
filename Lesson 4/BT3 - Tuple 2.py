b22eac91ad22c621dfa8048536078abea395b0ca
Pham Minh Duc <pmduc2012@gmail.com>
2015-09-24 16:27:36 -0700

    Update tuple

---
+++ Lesson 4/BT3 - Tuple 2.py
@@ -0,0 +1,13 @@
+numList = [9, 3, 2, 5, 6, 8, 11]
+num1 = []
+num2 = []
+for i in numList:
+    if i <= 5:
+        num1.append(i)
+    else:
+        num2.append(i)
+num1.sort()
+num2.sort()
+Result = (num1 , num2)
+
+print(Result)
